433d434cd1ad52eeef92beb6df6b30831a6f2dbe
berka38 <gladyotr123@gmail.com>
2025-01-08 13:45:16 +0300

    first commit

--- apps/home/views.py
+++ apps/home/views.py
@@ -2,8 +2,7 @@
 """
 Copyright (c) 2019 - present AppSeed.us
 """
-import matplotlib
-matplotlib.use('Agg')
+
 from django import template
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
@@ -17,11 +16,7 @@ from django.utils import timezone
 from django.contrib import messages
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.signal import spectrogram
-from scipy.fft import fft
-from mpl_toolkits.mplot3d import Axes3D
+
 import os
 from django.conf import settings
 
@@ -132,73 +127,8 @@ def job_detail(request):
 
 
 def generate_graphs(request):
-    # Kaydedilecek görüntülerin yolu
-    img_dir = os.path.join("apps/static", 'myapp/static/images')
-    if not os.path.exists(img_dir):
-        os.makedirs(img_dir)
-    # Initialize parameters
-    samplerate = 500  # in Hz
-    N = 1024  # data length
-    sinefreq1 = 20  # in Hz
-    sinefreq2 = 60  # in Hz
-    nfft = 64  # window size for each segment
-    noverlap = nfft // 2  # number of overlapping points (50%)
-
-    # Generate simulated signals with step changes in frequency
-    ts = np.arange(1, N//4 + 1) / samplerate  # time axis for each segment
-    data = np.concatenate([np.zeros(N//4), 
-                           np.sin(2 * np.pi * sinefreq1 * ts), 
-                           np.sin(2 * np.pi * sinefreq2 * ts), 
-                           np.zeros(N//4)])
-
-    taxis = np.arange(1, N+1) / samplerate  # time axis for whole data length
-
-    # --- Plot the signal ---
-    plt.figure()
-    plt.plot(taxis, data)
-    plt.xlim([taxis[0], taxis[-1]])
-    plt.xlabel('Time (s)')
-    plt.title('Signal with Step Changes in Frequency')
-    plt.savefig(os.path.join(img_dir, 'plot_signal.png'))
-    plt.close()
-
-    # --- FFT Spectrum ---
-    data_freq = fft(data, N)
-    Mag = np.abs(data_freq)
-    faxis = np.linspace(0, samplerate/2, N//2 + 1)
-
-    plt.figure()
-    plt.plot(faxis, Mag[:N//2 + 1])
-    plt.title('Spectral Analysis (FFT)')
-    plt.xlabel('Frequency (Hz)')
-    plt.ylabel('Magnitude')
-    plt.savefig(os.path.join(img_dir, 'plot_fft.png'))
-    plt.close()
-
-    # --- Spectrogram ---
-    f, t, Sxx = spectrogram(data, fs=samplerate, window='hamming', nperseg=nfft, noverlap=noverlap, nfft=nfft)
-
-    plt.figure()
-    plt.pcolormesh(t, f, np.abs(Sxx), shading='auto')
-    plt.xlabel('Time (s)')
-    plt.ylabel('Frequency (Hz)')
-    plt.title('Spectrogram (Surface Plot)')
-    plt.colorbar(label='Magnitude')
-    plt.savefig(os.path.join(img_dir, 'plot_spectrogram.png'))
-    plt.close()
-
-    # --- 3D Surface Plot ---
-    plt.figure()
-    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-    X, Y = np.meshgrid(t, f)
-    ax.plot_surface(X, Y, np.abs(Sxx), cmap='viridis', edgecolor='none')
-    ax.set_xlabel('Time (s)')
-    ax.set_ylabel('Frequency (Hz)')
-    ax.set_zlabel('Magnitude')
-    plt.title('Spectrogram (3D Surface Plot)')
-    plt.savefig(os.path.join(img_dir, 'plot_surface.png'))
-    plt.close()
-
+    
+    
     # Görselleri şablona gönder
     context = {
         'plots': [
